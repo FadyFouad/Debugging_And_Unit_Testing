fa5c93cffa71994d959d646fc498b865eced9979
fadyfouad <fady.fouad.a@gmail.com>
2019-06-22 16:37:42 +0200

    isChecking() & Before annotation

--- src/com/etaTech/BankAccountTest.java
+++ src/com/etaTech/BankAccountTest.java
@@ -1,27 +1,37 @@
 package com.etaTech;
 
-import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.*;
 
 public class BankAccountTest {
 
+    private BankAccount bankAccount ;
+    @org.junit.Before
+    public void setup(){
+        bankAccount = new BankAccount("Fady", "Fouad", 10_000_000,BankAccount.CHECKING);
+        System.out.println("Before test");
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount bankAccount = new BankAccount("Fady", "Fouad", 10_000_000);
         double balance = bankAccount.deposit(2_000_000, true);
         assertEquals(12_000_000, balance, 0);
     }
 
     @org.junit.Test
     public void withdraw() {
-        BankAccount bankAccount = new BankAccount("Fady", "Fouad", 10_000_000);
         bankAccount.withdraw(2_000_000,true);
         assertEquals(8_000_000,bankAccount.getBalance(),0);
     }
 
     @org.junit.Test
     public void getBalance() {
-        BankAccount bankAccount = new BankAccount("Fady", "Fouad", 10_000_000);
         bankAccount.deposit(2_000_000, true);
         assertEquals(12_000_000, bankAccount.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking(){
+        BankAccount bankAccount = new BankAccount("Fady", "Fouad", 10_000_000,BankAccount.CHECKING);
+        assertFalse( "Leave your message here",bankAccount.isChecking());
     }
 }
